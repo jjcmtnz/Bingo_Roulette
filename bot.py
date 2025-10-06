13745fdbdb3536d8063b772a58dc038905ad9e32
JJ <jjcmtnz@gmail.com>
2025-10-06 00:30:55 -0400

    latest fix

--- bot.py
+++ bot.py
@@ -1840,8 +1840,8 @@ CHALLENGE_INFO = {
              "• Your 5-man group must include at least 3 teammates.\n\n"
             "**Bonus Points:**\n"
             "• +4 for any team that completes a *5-man kill in 4:00 or less*\n"
-            "• +2 for the *fastest submitted 5-man completion time at The Nightmare*\n"
-            "• +2 for the *fastest submitted completion time at Phosani’s Nightmare*\n\n"
+            "• +2 for the *fastest submitted 5-man completion time* at The Nightmare*\n"
+            "• +2 for the *fastest submitted solo completion time* at Phosani’s Nightmare*\n\n"
             "**Expiration:**\n"
             "You have 48 hours to complete this challenge. Team Challenge #4 expires on <t:1760893200:F>.\n\n"
         ),
